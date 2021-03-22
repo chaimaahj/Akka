c47db523582dcff55a7e9d833316c0147b03b57f
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2021-03-22 14:42:04 +0100

    Add files via upload

--- src/Master.java
+++ src/Master.java
@@ -1,7 +1,15 @@
 import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
 
 
-public class Master {
+public class Master extends UntypedActor{
 	public ActorRef reducer1, reducer2;
+	public ActorRef mapper1, mapper2,mapper3;
+
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		
+		
+	}
 
 }
